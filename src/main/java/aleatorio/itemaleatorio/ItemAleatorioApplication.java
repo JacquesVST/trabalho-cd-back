b89512cffc35ddd6dc3556b5ce9bbbc3b80dcdc5
Luan Mantuani <luangmantuani@gmail.com>
2021-11-20 19:05:54 -0300

    projeto base

---
+++ src/main/java/aleatorio/itemaleatorio/ItemAleatorioApplication.java
@@ -0,0 +1,13 @@
+package aleatorio.itemaleatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemAleatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemAleatorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aleatorio/itemaleatorio/ItemAleatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package aleatorio.itemaleatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemAleatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
